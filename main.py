8c0f7300077eecf22806de5674db1950306dd211
Nick <nickj.joslyn@gmail.com>
2018-06-19 16:00:05 -0700

    Run the main with function calls

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # Nicholas Joslyn
 # Breakthrough Listen UC Berkeley SETI Intern 2018
 
-# Main program for quasi-real time spectrum estimates
+# Main program for quasi-real time spectrum estimates of BL RAW data
 
 import numpy as np
 import os
 import spectrumEstimators
 from header import extractHeader
+import time
+
+startTime = time.time()
 
 if __name__ == "__main__":
+
     #Memmap the RAW file and find the number of bytes
     inputFileName = "../../Downloads/blc05_guppi_58100_78802_OUMUAMUA_0011.0000.raw"
     readIn = np.memmap(inputFileName, dtype = 'int8', mode = 'r')
@@ -17,6 +21,8 @@ if __name__ == "__main__":
     #Initial location
     currentBytesPassed = 0
     blockNumber = 0
+
+    startTime
 
     while (currentBytesPassed < fileBytes):
 
@@ -34,10 +40,13 @@ if __name__ == "__main__":
         for CHANNEL in range(OBSNCHAN):
             centerFrequency = OBSFREQ + (np.abs(OBSBW)/2) - (CHANNEL + 0.5)*np.abs(CHAN_BW)
             spectrumEstimators.RAW_periodogram(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, TBIN)
-            # spectrumEstimators.RAW_periodogram(dataBuffer[CHANNEL,:,:], CHANNEL, TBIN, 'spectrum')
+            spectrumEstimators.RAW_periodogram(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, TBIN, 'spectrum')
             spectrumEstimators.RAW_FFT(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW)
-            # spectrumEstimators.RAW_FFT(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, 'power spectrum')
-            # spectrumEstimators.RAW_FFT(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, 'power density')
-
+            spectrumEstimators.RAW_FFT(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, 'power spectrum')
+            spectrumEstimators.RAW_FFT(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, 'power normalized')
+            spectrumEstimators.RAW_FFT(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, 'magnitude normalized')
+            spectrumEstimators.RAW_pyplot_magnitude(dataBuffer[CHANNEL,:,:], CHANNEL, centerFrequency, CHAN_BW, TBIN)
         del readIn
+        endTime = time.time()
+        print("Time:" + str(round(endTime - startTime)))
         exit()
