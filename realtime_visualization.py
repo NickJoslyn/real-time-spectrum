0bb548c3df83dcb0fc2f32e6512d7e85f5e48b4f
Nick <nickj.joslyn@gmail.com>
2018-07-08 22:59:57 -0700

    Fixed some plotting on export

--- realtime_visualization.py
+++ realtime_visualization.py
@@ -516,7 +516,7 @@ if __name__ == "__main__":
     OBSNCHAN = 64
     dualPolarization = 2
 
-    startTime = datetime.now().strftime('%H:%M:%S')
+    startTime = datetime.now().strftime('%H:%M')
 
     ########################
     ### Shell commands
@@ -652,7 +652,7 @@ if __name__ == "__main__":
 
     plt.close()
 
-    endTime = datetime.now().strftime('%H:%M:%S')
+    endTime = datetime.now().strftime('%H:%M')
 
     ####################################################################################
     ####################----EXPORT WATERFALLS----#######################################
@@ -693,16 +693,16 @@ if __name__ == "__main__":
             export_fig = plt.figure()
             plt.suptitle("blc" + str(export_bank + BANK_OFFSET) + str(export_node) + " | " + str(desiredFrequencyResolution/(10**6)) + " MHz, " + str(desiredTimeResolution*(10**3)) + " ms Resolution")
 
-            export_axis1 = plt.subplot2grid((14,6), (0, 0), colspan=2, rowspan=14)
+            export_axis1 = plt.subplot2grid((14,10), (0, 1), colspan=2, rowspan=14)
             export_axis1.set_title("X")
             export_axis1.set_xlabel("Frequency (MHz)")
-            export_axis1.set_ylabel("Time (Hours)")
+            #export_axis1.set_ylabel("Time (Hours)")
             export_axis1.margins(x=0)
 
-            export_axis2 = plt.subplot2grid((14,6), (0, 3), colspan=2, rowspan=14)
+            export_axis2 = plt.subplot2grid((14,10), (0, 6), colspan=2, rowspan=14)
             export_axis2.set_title("Y")
             export_axis2.set_xlabel("Frequency (MHz)")
-            export_axis2.set_ylabel("Time (Hours)")
+            #export_axis2.set_ylabel("Time (Hours)")
             export_axis2.margins(x=0)
             ########
 
